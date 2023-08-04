ded01a95ddcd7b27766a573f3b4e1c1730baa187
Chanrich <effortpower@outlook.com>
2023-08-04 11:32:29 +0800

    add photo and video

--- data/video.ts
+++ data/video.ts
@@ -1,15 +1,15 @@
 import { title } from "@site/docusaurus.config"
 
 export const Photo: Photo[] =[
-  {"title":"DSC_0458.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0458.MOV?Expires=1691116500&OSSAccessKeyId=TMP.3KdWkWJHTi98yJVSobgZ7qR3pWXv5tFpSmrhdfczKXCD8ZRPWCHXnfMx47F6MvmcgUGPcDrHBMdrDSqqLYC245LcbCJEXV&Signature=KLNQai6cXwbeP9E%2BxLOqXLepumw%3D"},
-  {"title":"DSC_0108.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0108.MOV?Expires=1691116500&OSSAccessKeyId=TMP.3KdWkWJHTi98yJVSobgZ7qR3pWXv5tFpSmrhdfczKXCD8ZRPWCHXnfMx47F6MvmcgUGPcDrHBMdrDSqqLYC245LcbCJEXV&Signature=vFeanrhdYEE0oCebmckjvM15toU%3D"},
-  {"title":"DSC_0189.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0189.MOV?Expires=1691116500&OSSAccessKeyId=TMP.3KdWkWJHTi98yJVSobgZ7qR3pWXv5tFpSmrhdfczKXCD8ZRPWCHXnfMx47F6MvmcgUGPcDrHBMdrDSqqLYC245LcbCJEXV&Signature=UYE35iAu0ipszCgvVnPuHQazjJc%3D"},
-  {"title":"DSC_0319.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0319.MOV?Expires=1691116500&OSSAccessKeyId=TMP.3KdWkWJHTi98yJVSobgZ7qR3pWXv5tFpSmrhdfczKXCD8ZRPWCHXnfMx47F6MvmcgUGPcDrHBMdrDSqqLYC245LcbCJEXV&Signature=dNCSx22bXMzrphsOnEBnush0%2F54%3D"},
-  {"title":"DSC_0399.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0399.MOV?Expires=1691116500&OSSAccessKeyId=TMP.3KdWkWJHTi98yJVSobgZ7qR3pWXv5tFpSmrhdfczKXCD8ZRPWCHXnfMx47F6MvmcgUGPcDrHBMdrDSqqLYC245LcbCJEXV&Signature=oH4p1RlR6OYkSjna0MV3qw3FjOk%3D"},
-  {"title":"DSC_0402.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0402.MOV?Expires=1691116500&OSSAccessKeyId=TMP.3KdWkWJHTi98yJVSobgZ7qR3pWXv5tFpSmrhdfczKXCD8ZRPWCHXnfMx47F6MvmcgUGPcDrHBMdrDSqqLYC245LcbCJEXV&Signature=ogPBnEFFojSEE0%2BtqhC5bAUROQk%3D"},
-  {"title":"DSC_0442.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0442.MOV?Expires=1691116500&OSSAccessKeyId=TMP.3KdWkWJHTi98yJVSobgZ7qR3pWXv5tFpSmrhdfczKXCD8ZRPWCHXnfMx47F6MvmcgUGPcDrHBMdrDSqqLYC245LcbCJEXV&Signature=w%2FHGTGobACNYLU0O7BQ2EF9HXcs%3D"},
-  {"title":"DSC_0444.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0444.MOV?Expires=1691116500&OSSAccessKeyId=TMP.3KdWkWJHTi98yJVSobgZ7qR3pWXv5tFpSmrhdfczKXCD8ZRPWCHXnfMx47F6MvmcgUGPcDrHBMdrDSqqLYC245LcbCJEXV&Signature=k0gCaVt7cX2Cnwnc7xOC%2BU8h4VU%3D"},
-  {"title":"DSC_0427.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0427.MOV?Expires=1691116500&OSSAccessKeyId=TMP.3KdWkWJHTi98yJVSobgZ7qR3pWXv5tFpSmrhdfczKXCD8ZRPWCHXnfMx47F6MvmcgUGPcDrHBMdrDSqqLYC245LcbCJEXV&Signature=lHKqZeRSs3IIMD0qb%2FROKUDlyjw%3D"}
+  {"title":"DSC_0108.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0108.MOV"},
+  {"title":"DSC_0189.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0189.MOV"},
+  {"title":"DSC_0319.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0319.MOV"},
+  {"title":"DSC_0399.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0399.MOV"},
+  {"title":"DSC_0402.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0402.MOV"},
+  {"title":"DSC_0427.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0427.MOV"},
+  {"title":"DSC_0442.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0442.MOV"},
+  {"title":"DSC_0444.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0444.MOV"},
+  {"title":"DSC_0458.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0458.MOV"}
 ]
   
 export type Photo = {

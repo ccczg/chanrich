d20e8f1b36623e8b66a64e8c35bb26c168e06dd9
chanrich <1418045110@qq.com>
2024-03-26 18:28:11 +0800

    hide photo

--- data/video.ts
+++ data/video.ts
@@ -1,15 +1,15 @@
 import { title } from "@site/docusaurus.config"
 
 export const Photo: Photo[] =[
-  {"title":"DSC_0108.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0108.MOV"},
-  {"title":"DSC_0189.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0189.MOV"},
-  {"title":"DSC_0319.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0319.MOV"},
-  {"title":"DSC_0399.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0399.MOV"},
-  {"title":"DSC_0402.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0402.MOV"},
-  {"title":"DSC_0427.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0427.MOV"},
-  {"title":"DSC_0442.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0442.MOV"},
-  {"title":"DSC_0444.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0444.MOV"},
-  {"title":"DSC_0458.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0458.MOV"}
+  // {"title":"DSC_0108.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0108.MOV"},
+  // {"title":"DSC_0189.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0189.MOV"},
+  // {"title":"DSC_0319.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0319.MOV"},
+  // {"title":"DSC_0399.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0399.MOV"},
+  // {"title":"DSC_0402.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0402.MOV"},
+  // {"title":"DSC_0427.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0427.MOV"},
+  // {"title":"DSC_0442.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0442.MOV"},
+  // {"title":"DSC_0444.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0444.MOV"},
+  // {"title":"DSC_0458.MOV","url":"https://mediaresource.oss-cn-beijing.aliyuncs.com/DSC_0458.MOV"}
 ]
   
 export type Photo = {
